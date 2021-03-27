ec3e7f532b2a0b40c1400883e4d8d297bc8b4fad
max <mpano91@gmail.com>
2021-03-27 14:14:03 +0300

    Resize image. done

---
+++ familiar/Vec2d.h
@@ -0,0 +1,115 @@
+#ifndef Vec2dD_H
+#define Vec2dD_H
+
+#include <cmath>
+
+template<typename T>
+class Vec2d {
+public:
+    Vec2d(): x(0), y(0) {}
+    Vec2d(T x, T y) : x(x), y(y) {}
+    Vec2d(const Vec2d& v) : x(v.x), y(v.y) {}
+
+    T x;
+    T y;
+
+    Vec2d& operator=(const Vec2d& v) {
+        x = v.x;
+        y = v.y;
+        return *this;
+    }
+
+    Vec2d operator+(Vec2d& v) {
+        return Vec2d(x + v.x, y + v.y);
+    }
+    Vec2d operator-(Vec2d& v) {
+        return Vec2d(x - v.x, y - v.y);
+    }
+
+    Vec2d& operator+=(Vec2d& v) {
+        x += v.x;
+        y += v.y;
+        return *this;
+    }
+    Vec2d& operator-=(Vec2d& v) {
+        x -= v.x;
+        y -= v.y;
+        return *this;
+    }
+
+    Vec2d operator+(double s) {
+        return Vec2d(x + s, y + s);
+    }
+    Vec2d operator-(double s) {
+        return Vec2d(x - s, y - s);
+    }
+    Vec2d operator*(double s) {
+        return Vec2d(x * s, y * s);
+    }
+    Vec2d operator/(double s) {
+        return Vec2d(x / s, y / s);
+    }
+
+
+    Vec2d& operator+=(double s) {
+        x += s;
+        y += s;
+        return *this;
+    }
+    Vec2d& operator-=(double s) {
+        x -= s;
+        y -= s;
+        return *this;
+    }
+    Vec2d& operator*=(double s) {
+        x *= s;
+        y *= s;
+        return *this;
+    }
+    Vec2d& operator/=(double s) {
+        x /= s;
+        y /= s;
+        return *this;
+    }
+
+    void set(T x, T y) {
+        this->x = x;
+        this->y = y;
+    }
+
+    void rotate(double deg) {
+        double theta = deg / 180.0 * M_PI;
+        double c = cos(theta);
+        double s = sin(theta);
+        double tx = x * c - y * s;
+        double ty = x * s + y * c;
+        x = tx;
+        y = ty;
+    }
+
+    Vec2d& normalize() {
+        if (length() == 0) return *this;
+        *this *= (1.0 / length());
+        return *this;
+    }
+
+    float dist(Vec2d v) const {
+        Vec2d d(v.x - x, v.y - y);
+        return d.length();
+    }
+
+    float length() const {
+        return std::sqrt(x * x + y * y);
+    }
+
+    static float dot(Vec2d v1, Vec2d v2) {
+        return v1.x * v2.x + v1.y * v2.y;
+    }
+    static float cross(Vec2d v1, Vec2d v2) {
+        return (v1.x * v2.y) - (v1.y * v2.x);
+    }
+
+};
+
+
+#endif // Vec2dD_H
--- familiar/moveitem.cpp
+++ familiar/moveitem.cpp
@@ -2,6 +2,8 @@
 #include "moveitem.h"
 #include <QPen>
 
+
+#include "Vec2d.h"
 #include "Logger.h"
 
 extern Logger logger;
@@ -47,37 +49,114 @@ void MovableCircle::mousePressEvent(QGraphicsSceneMouseEvent *event)
     _shiftMouseCoords = this->pos() - mapToScene(event->pos());
     this->setCursor(QCursor(Qt::ClosedHandCursor));
 }
-
+#define LOGPRINT(name) qDebug()<<#name<<"= "<<name;
 void MovableCircle::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
 {
     auto pos = mapToScene(event->pos() + _shiftMouseCoords);
 
-    qreal xl = (pos.x() == 0) ? .1 : pos.x();
-    qreal yl = (pos.y() == 0) ? .1 : pos.y();
+    qreal xl = pos.x();
+    qreal yl = pos.y();
+    auto rect = parentItem()->boundingRect();
+
+    QPointF a(rect.x(), rect.y());
+    QPointF b(rect.x() + rect.width(), rect.y());
+    QPointF c(rect.x() + rect.width(), rect.y() + rect.height());
+    QPointF d(rect.x(), rect.y() + rect.height());
 
     qreal arl = qAbs(xl / yl);
 
-    if (circlePos_ == eBottomRight) {
-        if (arl > aspectRatio_) {
-            pos.setX(yl * aspectRatio_);
-        } else {
-            pos.setY(xl / aspectRatio_);
+    if (circlePos_ == eTopRight) {
+        Vec2d dc(c.x()-d.x(), c.y()-d.y());
+        Vec2d cb(b.x()-c.x(), b.y()-c.y());
+        Vec2d db(b.x()-d.x(), b.y()-d.y());
+        Vec2d dk(pos.x()-d.x(), pos.y()-d.y());
 
-        }
+        auto dc_len = dc.length();
+        auto cb_len = cb.length();
+        auto db_len = db.length();
+        auto dk_len = dk.length();
+
+        auto dkdb_dot = Vec2d<qreal>::dot(db, dk);
+        auto cos_kdb = dkdb_dot/(db_len*dk_len);
+        auto dd2_len = dk_len * cos_kdb;
+
+        auto x =(dd2_len * dc_len) / (std::sqrt(cb_len * cb_len + dc_len * dc_len));
+        auto y = std::sqrt(dd2_len * dd2_len - x * x);
+
+        if (x < 10 || y < 10) return;
+        pos.setX(d.x()+x);
+        pos.setY(d.y()-y);
+    }
+
+    if (circlePos_ == eBottomRight) {
+        Vec2d ad(d.x()-a.x(), d.y()-a.y());
+        Vec2d dc(c.x()-d.x(), c.y()-d.y());
+        Vec2d ac(c.x()-a.x(), c.y()-a.y());
+        Vec2d ak(pos.x()-a.x(), pos.y()-a.y());
+
+        auto ad_len = ad.length();
+        auto dc_len = dc.length();
+        auto ac_len = ac.length();
+        auto ak_len = ak.length();
+
+        auto akac_dot = Vec2d<qreal>::dot(ac, ak);
+        auto cos_kac = akac_dot/(ac_len*ak_len);
+        auto ad2_len = ak_len * cos_kac;
+
+        auto x =(ad2_len * dc_len) / (std::sqrt(ad_len * ad_len + dc_len * dc_len));
+        auto y = std::sqrt(ad2_len * ad2_len - x * x);
+
+        if (x < 10 || y < 10) return;
+        pos.setX(a.x()+x);
+        pos.setY(a.y()+y);
     }
 
     if (circlePos_ == eTopLeft) {
-        LOG_WARNING(logger, "Circle Pos: ", circlePos_, ", ", pos.x(), " ", pos.y());
-        LOG_WARNING(logger, "Init Aspect Ratio: ", aspectRatio_, ", Current AspectRatio:", arl);
-        if (arl > aspectRatio_) {
-            LOG_DEBUG(logger, "> Before: ", pos.x(), ", ", pos.y());
-            pos.setY(xl / aspectRatio_);
-            LOG_DEBUG(logger, "> After: ", pos.x(), ", ", pos.y());
-        } else {
-            LOG_DEBUG(logger, "< Before: ", pos.x(), ", ", pos.y());
-            pos.setX(yl * aspectRatio_);
-            LOG_DEBUG(logger, "< After: ", pos.x(), ", ", pos.y());
-        }
+        qDebug()<<this->parentItem()->boundingRect();
+        Vec2d cb(b.x()-c.x(), b.y()-c.y());
+        Vec2d ba(a.x()-b.x(), a.y()-b.y());
+        Vec2d ca(a.x()-c.x(), a.y()-c.y());
+        Vec2d ck(pos.x()-c.x(), pos.y()-c.y());
+
+        auto cb_len = cb.length();
+        auto ba_len = ba.length();
+        auto ca_len = ca.length();
+        auto ck_len = ck.length();
+
+        auto ckca_dot = Vec2d<qreal>::dot(ca, ck);
+        auto cos_kca = ckca_dot/(ca_len*ck_len);
+        auto cd2_len = ck_len * cos_kca;
+
+        auto y =(cd2_len * cb_len) / (std::sqrt(ba_len * ba_len + cb_len * cb_len));
+        auto x = std::sqrt(cd2_len * cd2_len - y * y);
+
+        if (x < 10 || y < 10) return;
+        pos.setX(c.x()-x);
+        pos.setY(c.y()-y);
+    }
+
+    if (circlePos_ == eBottomLeft) {
+        qDebug()<<this->parentItem()->boundingRect();
+        Vec2d ba(a.x()-b.x(), a.y()-b.y());
+        Vec2d ad(d.x()-a.x(), d.y()-a.y());
+        Vec2d bd(d.x()-b.x(), d.y()-b.y());
+        Vec2d bk(pos.x()-b.x(), pos.y()-b.y());
+
+        auto ba_len = ba.length();
+        auto ad_len = ad.length();
+        auto bd_len = bd.length();
+        auto bk_len = bk.length();
+
+        auto bkbd_dot = Vec2d<qreal>::dot(bd, bk);
+        auto cos_kdb = bkbd_dot/(bd_len*bk_len);
+        auto bd2_len = bk_len * cos_kdb;
+
+        auto x =(bd2_len * ba_len) / (std::sqrt(ad_len * ad_len + ba_len * ba_len));
+        auto y = std::sqrt(bd2_len * bd2_len - x * x);
+
+        if (x < 10 || y < 10) return;
+        pos.setX(b.x()-x);
+        pos.setY(b.y()+y);
     }
 
     setPos(pos);
@@ -98,6 +177,7 @@ MoveItem::MoveItem(uint64_t& zc, QGraphicsItem *parent) :
     QGraphicsItem(parent), zCounter_(zc)
 {
     setZValue(zCounter_);
+//    qimage_ = QImage("bender.png");
     qimage_ = QImage("kot.png");
 //    auto qimage = QImage("kot.jpg");
 //    qInfo() << qimage.width() << ' ' <<qimage.height();
