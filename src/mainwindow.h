2f05cd31d00a815882a0ad7723e505fe6c6f22bd
max <mpano91@gmail.com>
2021-05-20 17:48:05 +0300

    change ui

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -8,6 +8,7 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
+class QFileDialog;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -19,11 +20,18 @@ public:
      */
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+public slots:
+    void onFilterSelected();
+private slots:
+    void on_action_open_triggered();
+    void on_action_saveas_triggered();
 
 private:
     Ui::MainWindow *ui;
     uchar* data_;
     QTimer* mainTimer_;
     CanvasView* canvasWidget;
+    QFileDialog* fileDialog_;
+    std::unordered_map<QString, QString> fileExt_;
 };
 #endif // MAINWINDOW_H
