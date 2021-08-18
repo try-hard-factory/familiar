015ac5cfc716019c1d1a28c9224a1dacb924afeb
max <mpano91@gmail.com>
2021-08-18 11:09:27 +0300

    Part of feature FileActions class

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -46,9 +46,9 @@ MainWindow::MainWindow(QWidget *parent)
 //    canvasWidget->addImage("kot1.png", {250,50-x});
 //    canvasWidget->addImage("kot1.png", {450,50-x});
 
-//    canvasWidget->addImage("kot1.png", {200-x, 150-x});
-//    canvasWidget->addImage("kot1.png", {450-x, 50-x});
-//    canvasWidget->addImage("kot.png", {700-x, 0-x});
+    canvasWidget->addImage("kot1.png", {200-x, 150-x});
+    canvasWidget->addImage("kot1.png", {450-x, 50-x});
+    canvasWidget->addImage("kot.png", {700-x, 0-x});
 
 //    canvasWidget->addImage("bender.png", {1300,1300});
 
@@ -68,8 +68,56 @@ MainWindow::~MainWindow()
     delete canvasWidget;
 }
 
+void MainWindow::openFile()
+{
+    int ret = -1;
+    if (projectSettings_->modified() == true) {
+        ret = messageShow("Warning!", "You have unsaved documents!\n"
+                "Do you wish to continue?", QMessageBox::Yes | QMessageBox::No);
+    }
 
-void MainWindow::on_action_saveas_triggered()
+    if (ret == QMessageBox::No) return;
+
+    cleanupWorkplace();
+
+    fileDialog_->setAcceptMode(QFileDialog::AcceptMode::AcceptOpen);
+    if (fileDialog_->exec() == QDialog::Accepted) {
+        qDebug()<< fileDialog_->selectedFiles().value(0);
+        QString file_name(fileDialog_->selectedFiles().value(0));
+        QFileInfo fi(file_name);
+        projectSettings_->title(fi.fileName());
+        projectSettings_->path(fi.absoluteFilePath());
+        projectSettings_->modified(false);
+
+        fml_file_buffer::open_file(canvasWidget, file_name);
+    }
+}
+
+void MainWindow::saveFile(const QString& path)
+{
+    QString header_= QString::fromStdString(fml_file_buffer::create_header(canvasWidget));
+    QByteArray byteHeader = header_.toUtf8();
+    QByteArray payload = fml_file_buffer::create_payload(canvasWidget);
+    fml_file_buffer::save_to_file(path, byteHeader, payload);
+}
+
+void MainWindow::saveFile()
+{
+    QFile file(projectSettings_->path());
+    if (!file.exists()) {
+        saveFileAs();
+        return;
+    }
+
+    QString out_file(projectSettings_->path());
+    projectSettings_->title(projectSettings_->title());
+
+    saveFile(out_file);
+
+    projectSettings_->modified(false);
+}
+
+void MainWindow::saveFileAs()
 {
     fileDialog_->setAcceptMode(QFileDialog::AcceptMode::AcceptSave);
 
@@ -83,105 +131,94 @@ void MainWindow::on_action_saveas_triggered()
         projectSettings_->title(fi.fileName());
         projectSettings_->path(fi.absoluteFilePath());
 
-        qDebug()<<out_file;
-        qDebug()<<fileDialog_->directory();
-        QString header_=QString::fromStdString(fml_file_buffer::create_header(canvasWidget));
-        QByteArray byteHeader = header_.toUtf8();
-        qDebug()<<"Header "<<header_;
-        QByteArray payload = fml_file_buffer::create_payload(canvasWidget);
+        saveFile(out_file);
 
-        fml_file_buffer::save_to_file(out_file, byteHeader, payload);
+        projectSettings_->modified(false);
     }
+}
+
+void MainWindow::quitProject()
+{
+    if (projectSettings_->modified() == false) {
+        QApplication::quit();
+    } else {
+        int ret = messageShow("Warning!", "You have unsaved documents!", QMessageBox::Save | QMessageBox::Cancel);
+
+        if (ret == QMessageBox::Save) {
+            saveFile();
+            QApplication::quit();
+        } else if (ret == QMessageBox::Cancel) {
+            return;
+        }
+    }
+}
+
+void MainWindow::cleanupWorkplace()
+{
+    canvasWidget->cleanupWorkplace();
+}
+
+void centerWidget(QWidget *widget, QWidget *host = Q_NULLPTR) {
+    if (!host) {
+        host = widget->parentWidget();
+    }
+
+    if (host) {
+        auto hostRect = host->geometry();
+        auto point = hostRect.center() - QPoint(widget->baseSize().width()/2,
+                                                widget->baseSize().height()/2);
+        widget->move(point);
+    } else {
+        QRect screenGeometry = QGuiApplication::screens()[0]->geometry();
+        int x = (screenGeometry.width() - widget->width()) / 2;
+        int y = (screenGeometry.height() - widget->height()) / 2;
+        widget->move(x, y);
+    }
+}
+
+
+int MainWindow::messageShow(const QString& title, const QString& text, QMessageBox::StandardButtons b)
+{
+    QMessageBox msg;
+    msg.setBaseSize(QSize(300,140));// I don't think that it is correct way.
+                                    // But I have to use it because QMessageBox::size()
+                                    // method returns the wrong value that I expect = (640x480)
+                                    // I think i'll create custom messages window
+    msg.setWindowTitle(title);
+    msg.setIcon(QMessageBox::Warning);
+    msg.setText(text);
+
+    msg.setStandardButtons(b);
+    msg.setDefaultButton(QMessageBox::No);
+
+    centerWidget(&msg, this);
+
+    return msg.exec();
+}
+
+
+void MainWindow::on_action_saveas_triggered()
+{
+    saveFileAs();
 }
 
 
 void MainWindow::on_action_open_triggered()
 {
-    fileDialog_->setAcceptMode(QFileDialog::AcceptMode::AcceptOpen);
-    if (fileDialog_->exec() == QDialog::Accepted) {
-        qDebug()<< fileDialog_->selectedFiles().value(0);
-        QString file_name(fileDialog_->selectedFiles().value(0));
-        QFileInfo fi(file_name);
-        projectSettings_->title(fi.fileName());
-        projectSettings_->path(fi.absoluteFilePath());
-        projectSettings_->change(false);
-
-        fml_file_buffer::open_file(canvasWidget, file_name);
-    }
+    openFile();
 }
 
 void MainWindow::on_action_quit_triggered()
 {
-    qDebug()<<"on_action_quit_triggered "<<projectSettings_->change();
-    if (projectSettings_->change() == false) {
-        QApplication::quit();
-    } else {
-        fileDialog_->setAcceptMode(QFileDialog::AcceptMode::AcceptSave);
-
-        if (fileDialog_->exec() == QDialog::Accepted) {
-            QString out_file(fileDialog_->selectedFiles().value(0));
-            if (!out_file.contains(fileExt_.at(fileDialog_->selectedNameFilter()), Qt::CaseInsensitive)) {
-                out_file.append(fileExt_.at(fileDialog_->selectedNameFilter()));
-            }
-
-            QFileInfo fi(out_file);
-            projectSettings_->title(fi.fileName());
-            projectSettings_->path(fi.absoluteFilePath());
-
-            qDebug()<<out_file;
-            qDebug()<<fileDialog_->directory();
-            QString header_=QString::fromStdString(fml_file_buffer::create_header(canvasWidget));
-            QByteArray byteHeader = header_.toUtf8();
-            qDebug()<<"Header "<<header_;
-            QByteArray payload = fml_file_buffer::create_payload(canvasWidget);
-
-            fml_file_buffer::save_to_file(out_file, byteHeader, payload);
-            QApplication::quit();
-        } else {//cancel
-
-        }
-    }
+    quitProject();
 }
 
 void MainWindow::on_action_save_triggered()
 {
-    if (projectSettings_->isDefaultProjectName()) {
-        fileDialog_->setAcceptMode(QFileDialog::AcceptMode::AcceptSave);
+    saveFile();
+}
 
-        if (fileDialog_->exec() == QDialog::Accepted) {
-            QString out_file(fileDialog_->selectedFiles().value(0));
-            if (!out_file.contains(fileExt_.at(fileDialog_->selectedNameFilter()), Qt::CaseInsensitive)) {
-                out_file.append(fileExt_.at(fileDialog_->selectedNameFilter()));
-            }
-
-            QFileInfo fi(out_file);
-            projectSettings_->title(fi.fileName());
-            projectSettings_->path(fi.absoluteFilePath());
-
-            qDebug()<<out_file;
-            qDebug()<<fileDialog_->directory();
-            QString header_=QString::fromStdString(fml_file_buffer::create_header(canvasWidget));
-            QByteArray byteHeader = header_.toUtf8();
-            qDebug()<<"Header "<<header_;
-            QByteArray payload = fml_file_buffer::create_payload(canvasWidget);
-
-            fml_file_buffer::save_to_file(out_file, byteHeader, payload);
-
-            projectSettings_->change(false);
-        } else {//cancel
-
-        }
-    } else {
-        QString out_file(projectSettings_->path());
-        projectSettings_->title(projectSettings_->title());
-
-        QString header_=QString::fromStdString(fml_file_buffer::create_header(canvasWidget));
-        QByteArray byteHeader = header_.toUtf8();
-        qDebug()<<"Header "<<header_;
-        QByteArray payload = fml_file_buffer::create_payload(canvasWidget);
-
-        fml_file_buffer::save_to_file(out_file, byteHeader, payload);
-
-        projectSettings_->change(false);
-    }
+void MainWindow::on_action_settings_triggered()
+{
+cleanupWorkplace();
 }
