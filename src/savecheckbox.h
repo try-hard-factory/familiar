ea03b9e0d3a08e048fa087f82a85f238acb25c24
max <mpano91@gmail.com>
2021-09-05 15:11:47 +0300

    custom save checkbox

---
+++ src/savecheckbox.h
@@ -0,0 +1,18 @@
+#ifndef SAVECHECKBOX_H
+#define SAVECHECKBOX_H
+
+#include <QCheckBox>
+
+class SaveCheckBox : public QCheckBox
+{
+    Q_OBJECT
+public:
+    SaveCheckBox(int id, QWidget *parent = 0);
+    ~SaveCheckBox() = default;
+    int id() const noexcept { return id_; };
+
+private:
+    int id_ = 0;
+};
+
+#endif // SAVECHECKBOX_H
