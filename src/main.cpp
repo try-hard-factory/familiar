22d9a05361b63f3440b7d03aa5fc737df6a7315b
max <mpano91@gmail.com>
2021-09-05 15:34:54 +0300

    mainpage doxygen

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,33 @@
 #include <QApplication>
 #include <Logger.h>
 
+/*!
+ * \~russian \mainpage RU
+ *
+ * \~russian \section intro_sec Introduction
+ *
+ * \~russian This is the introduction.
+ *
+ * \~russian \section install_sec Installation
+ *
+ * \~russian \subsection step1 Step 1: Opening the box
+ *
+ * \~russian etc...
+ *
+ * \~english \mainpage ENG
+ *
+ * \~english \section intro_sec Introduction
+ *
+ * \~english This is the introduction.
+ *
+ * \~english \section install_sec Installation
+ *
+ * \~english \subsection step1 Step 1: Opening the box
+ *
+ * \~english etc...
+ */
+
+
 Logger logger;
 
 int main(int argc, char *argv[])
