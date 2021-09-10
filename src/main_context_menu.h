fd1b1c2241a4b2bd138b98087213b54b564adc3c
max <mpano91@gmail.com>
2021-09-10 09:27:38 +0300

    add contextmenu

---
+++ src/main_context_menu.h
@@ -0,0 +1,21 @@
+#ifndef MAINCONTEXTMENU_H
+#define MAINCONTEXTMENU_H
+
+#include <QMenu>
+
+class MainWindow;
+
+class MainContextMenu : public QMenu
+{
+    Q_OBJECT
+public:
+    MainContextMenu(MainWindow& wm, QWidget *parent = nullptr);
+    ~MainContextMenu();
+private slots:
+    void quit();
+private:
+    MainWindow& mainwindow_;
+    QAction* quit_action = nullptr;
+};
+
+#endif // MAINCONTEXTMENU_H
