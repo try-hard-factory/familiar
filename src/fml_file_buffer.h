b1e280dad0dcc116126fc4f3e668d1100a1c4747
max <mpano91@gmail.com>
2021-09-11 11:46:43 +0300

    rewrite binary fime. add simple compress(but work very bad)

--- src/fml_file_buffer.h
+++ src/fml_file_buffer.h
@@ -27,25 +27,17 @@ class fml_file_buffer
 public:
 
     template<typename T>
-    static std::string create_header(T* obj) {
-        return obj->fml_header();
-    }
-
-    template<typename T>
     static QByteArray create_payload(T* obj) {
         return obj->fml_payload();
     }
 
-    static void save_to_file(QString filename, QByteArray& header, QByteArray& payload) {
+    static void save_to_file(QString filename, QByteArray& payload) {
         QFile file(filename);
         if (!file.open(QIODevice::WriteOnly | QIODevice::Truncate)) {
             qDebug()<<"Unnable to save file";
             return;
         }
 
-        uint32_t hs = header.size();
-        file.write((const char*)&hs, sizeof (hs));
-        file.write(header);
         file.write(payload, payload.size());
 
         file.close();
@@ -58,34 +50,25 @@ public:
             return;
         }
 
-        uint32_t hs = 0;
-        file.read((char *)&hs, sizeof(hs));
+        QDataStream stream (&file);
 
-        QByteArray header_b = file.read(hs);
-        QString header(header_b);
-
-        QStringList list = header.split(';');
-        for (auto& it : list) {
-            if (it.isEmpty()) break;
-//            qDebug()<<it;
-            QStringList img_info = it.split(',');
-            double x = img_info[0].toDouble();
-            double y = img_info[1].toDouble();
-            double h = img_info[2].toDouble();
-            double w = img_info[3].toDouble();
-            double bh = img_info[4].toDouble();
-            double bw = img_info[5].toDouble();
-            size_t sizepix = img_info[6].toUInt();
-            QImage::Format format = (QImage::Format)img_info[7].toUInt();
-//            qDebug()<<x;
-//            qDebug()<<y;
-//            qDebug()<<w;
-//            qDebug()<<h;
-//            qDebug()<<sizepix;
-//            qDebug()<<format;
-            QByteArray img_payload = file.read(sizepix);
-            QRect br = QRect(x, y, bw, bh);
-            obj->addImage(img_payload, w, h, br, w*4, format);
+        qint16 count = 0;
+        stream >> count;
+        for (int i = 0; i < count; ++i) {
+            QPointF scenePos;
+            stream >>scenePos;
+            qint32 h,w;
+            stream >> h >> w;
+            QRectF br;
+            stream >> br;
+            quint16 format;
+            stream >> format;
+            quint64 size;
+            stream >> size;
+            QByteArray zip_img_payload = file.read(size);
+            QByteArray img_payload = qUncompress(zip_img_payload);
+            QRect br_ = QRect(scenePos.x(), scenePos.y(), w, h);
+            obj->addImage(img_payload, w, h, br_, w*4, QImage::Format(format));
         }
 
         file.close();
